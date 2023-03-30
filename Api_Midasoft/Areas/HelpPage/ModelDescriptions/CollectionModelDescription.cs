f993277025096c05734038f5e52ea2c54ced5ed2
JHONATAN STEVEN CARRILLO MEJIA <89527312+CarrilloJhonatan@users.noreply.github.com>
2023-03-30 18:12:51 -0500

    Agregar archivos de proyecto.

---
+++ Api_Midasoft/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Api_Midasoft.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Api_Midasoft/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Api_Midasoft.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
