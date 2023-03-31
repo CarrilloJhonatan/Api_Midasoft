31ca27db1f6b3dd01f103b4c617df686781155f3
JHONATAN STEVEN CARRILLO MEJIA <89527312+CarrilloJhonatan@users.noreply.github.com>
2023-03-30 21:07:38 -0500

    Cambios v1

---
+++ Api_Midasoft/Controllers/LogueoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api_Midasoft.Controllers
+{
+    [AllowAnonymous]
+    public class LogueoController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
